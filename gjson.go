9fbfbfcdf4e2e2ef3ae9e743097dae6aa17ebd46
cnbattle <qiaicn@gmail.com>
2024-03-14 09:35:33 +0800

    gjson

--- gjson.go
+++ gjson.go
@@ -14,3 +14,14 @@ func JsonParse(json string) gjson.Result {
 func JsonGet(json, path string) gjson.Result {
 	return gjson.Get(json, path)
 }
+
+// ParseJson parses the json and returns a result.
+func ParseJson(json string) gjson.Result {
+	return gjson.Parse(json)
+
+}
+
+// GetJson searches json for the specified path.
+func GetJson(json, path string) gjson.Result {
+	return gjson.Get(json, path)
+}
